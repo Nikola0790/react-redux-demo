45f62cd83379f8a95d2bef57c8c25c2121b3e958
Nikola0790 <nikola.drcalic@gmail.com>
2022-01-25 14:24:32 +0100

    added cake container

---
+++ src/components/cakeContainer.jsx
@@ -0,0 +1,25 @@
+import actionBuyCake from "../redux/cake/cakeActions";
+import { connect } from "react-redux";
+
+function CakeContainer(props) {
+  return (
+    <div>
+      <h2>Number of cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(actionBuyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
